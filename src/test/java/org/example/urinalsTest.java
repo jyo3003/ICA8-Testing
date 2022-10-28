6048945869eabf08e0f6da249bdf19c1d1e4e6f5
jyo3003 <jkantip1@asu.edu>
2022-10-27 20:24:42 -0700

    Added a fail JUnit Test Case

--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -5,4 +5,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class urinalsTest {
+    @Test
+    public void TestCase() { //created a JUnit test case that fails
+        System.out.println("====== Jyothirmai Kantipudi == TEST ONE EXECUTED =======");
+        assertEquals(1, urinals.numberOfUrinals("1001"));
+    }
 }
