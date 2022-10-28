ab3b3b13aaf53c9a6432c36467167e4d4834b280
jyo3003 <jkantip1@asu.edu>
2022-10-27 22:25:01 -0700

    Correcting my code to pass the test

--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -8,6 +8,6 @@ class urinalsTest {
     @Test
     public void TestCase() { //created a JUnit test case that fails
         System.out.println("====== Jyothirmai Kantipudi == TEST ONE EXECUTED =======");
-        assertEquals(1, urinals.numberOfUrinals("1001"));
+        assertEquals(0, urinals.numberOfUrinals("1001"));
     }
 }
