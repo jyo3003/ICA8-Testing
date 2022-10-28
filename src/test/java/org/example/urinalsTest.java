01d31fa7cfd472c8d1ad19416dda288a14f798a7
jyo3003 <jkantip1@asu.edu>
2022-10-27 22:29:05 -0700

    Added 2nd test case

--- src/test/java/org/example/urinalsTest.java
+++ src/test/java/org/example/urinalsTest.java
@@ -6,8 +6,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class urinalsTest {
     @Test
-    public void TestCase() { //created a JUnit test case that fails
+    public void TestCaseOne() { //created a JUnit test case that fails
         System.out.println("====== Jyothirmai Kantipudi == TEST ONE EXECUTED =======");
         assertEquals(0, urinals.numberOfUrinals("1001"));
     }
+
+    @Test
+    public void TestCaseTwo() { //created a JUnit test case that fails
+        System.out.println("====== Jyothirmai Kantipudi == TEST TWO EXECUTED =======");
+        assertEquals(0, urinals.numberOfUrinals("1101"));
+    }
+    
 }
