c11ebfdb1f597c30db0f95adb95f55866f6eae2f
jyo3003 <jkantip1@asu.edu>
2022-10-27 18:48:48 -0700

    Added code skeletons

---
+++ src/main/java/org/example/urinals.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class urinals {
+
+    //checking if the string is valid
+    public boolean isValid(String str){
+        return !str.equals(null);
+    }
+
+    public static void main(String[] args){
+        System.out.println("Created main class!");
+    }
+}
